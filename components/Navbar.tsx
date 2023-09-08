60530ac33343197c7c904c4fa5b441ffe5ba9dbf
HarshPatel682 <patel4harsh@gmail.com>
2023-09-08 11:47:25 -0400

    created navbar and footer

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,12 @@
-import Image from 'next/image'
-import Link from 'next/link'
+import { NavLinks } from '@/constants';
+import Image from 'next/image';
+import Link from 'next/link';
+import AuthProviders from './AuthProviders';
+
 
 const Navbar = () => {
+    const session = {};
+
     return (
         <nav className="flexBetween navbar">
             <div className="flex-1 flexStart gap-10">
@@ -12,6 +17,27 @@ const Navbar = () => {
                     height={43}
                     alt="Dclone"
                 />
+
+                <ul className="xl:flex hidden text-small gap-7">
+                    {NavLinks.map((link) => (
+                        <Link href={link.href} key={link.key}>
+                            {link.text}
+                        </Link>
+                    ))}
+                </ul>
+            </div>
+            <div className="flexCenter gap-4">
+                {session ? (
+                    <>
+                    UserPhoto
+
+                    <Link href="/create-project">
+                        Share work
+                    </Link>
+                    </>
+                ) : (
+                    <AuthProviders />
+                )}
             </div>
         </nav>
     )
